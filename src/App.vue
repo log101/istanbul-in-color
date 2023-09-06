2fdbaf7c9b47a29c8163885bdb321b4f67a4fc12
log101 <50502021+log101@users.noreply.github.com>
2023-09-06 17:18:57 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorld msg="Istanbul in Color" />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  msg: String,
+});
+
+function displayFile(input, ev) {
+  var file = input.target.files[0];
+
+  if (file) {
+    var reader = new FileReader();
+    var image = document.getElementById('uploadedFile');
+
+    reader.onload = function (e) {
+      image.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <input id="uploadInput" type="file" @change="displayFile" />
+
+  <!-- Container to display the uploaded file -->
+  <div id="fileContainer">
+    <img id="uploadedFile" style="max-width: 300px; max-height: 300px" />
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
